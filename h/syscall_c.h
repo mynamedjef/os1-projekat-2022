6081f98a2c16c725367a39946e8336f7e62a63a6
fedja <fedjamladenovic@gmail.com>
2022-05-09 13:07:13 +0200

    dodaj zadate interfejse

---
+++ h/syscall_c.h
@@ -0,0 +1,49 @@
+//
+// Created by djafere on 5/8/22.
+//
+
+#ifndef _syscall_c
+#define _syscall_c
+
+// ============== memorija ===============
+
+void *mem_alloc(size_t size);
+int mem_free(void*);
+
+// ============== niti ===================
+
+class _thread;
+typedef _thread *thread_t;
+int thread_create(
+	thread_t *handle,
+	void(*start_routine)(void*),
+	void *arg
+);
+
+int thread_exit();
+void thread_dispatch();
+
+// ============== semafori ===============
+
+class _sem;
+typedef _sem *sem_t;
+int sem_open(
+	sem_t *handle,
+	unsigned init
+);
+
+int sem_close(sem_t handle);
+int sem_wait(sem_t id);
+int sem_signal(sem_t id);
+
+// ============== utils ==================
+
+typedef unsigned long time_t;
+int time_sleep(time_t);
+
+const int EOF = -1;
+char getc();
+void putc(char);
+
+
+#endif //_syscall_c
