ecb225ed4f652330fa8b245cedce68aa5320d221
Fedja Mladenovic <fedjamladenovic@gmail.com>
2022-06-24 12:46:01 +0200

    dodat Semaphore iz C++ API

---
+++ src/Semaphore.cpp
@@ -0,0 +1,25 @@
+//
+// Created by djafere on 6/24/22.
+//
+
+#include "../h/syscall_cpp.hpp"
+
+Semaphore::Semaphore(unsigned init)
+{
+    sem_open(&myHandle, init);
+}
+
+Semaphore::~Semaphore()
+{
+    sem_close(myHandle);
+}
+
+int Semaphore::wait()
+{
+    return sem_wait(myHandle);
+}
+
+int Semaphore::signal()
+{
+    return sem_signal(myHandle);
+}
