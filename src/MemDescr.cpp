43090da33a9010844430bd120cbff21042413db1
Fedja Mladenovic <fedjamladenovic@gmail.com>
2022-07-11 20:09:06 +0200

    tab -> space

--- src/MemDescr.cpp
+++ src/MemDescr.cpp
@@ -7,90 +7,90 @@
 
 void insert(MemDescr **head_ptr, MemDescr *node, MemChunkStatus st)
 {
-	// ako je node=NULL, odmah se vrati
-	if (!node) return;
-	node->status = st;
+    // ako je node=NULL, odmah se vrati
+    if (!node) return;
+    node->status = st;
 
-	// ako je head=NULL (odnosno ako nije bilo čvorova u listi do sad), head postaje node
-	if (!(*head_ptr)) {
-		*head_ptr = node;
-		return;
-	}
+    // ako je head=NULL (odnosno ako nije bilo čvorova u listi do sad), head postaje node
+    if (!(*head_ptr)) {
+        *head_ptr = node;
+        return;
+    }
 
-	// ako node treba da stoji ispred head-a, preveži ih i node postaje head
-	MemDescr *head = *head_ptr;
-	if (node < head) {
-		node->next = head;
-		head->prev = node;
+    // ako node treba da stoji ispred head-a, preveži ih i node postaje head
+    MemDescr *head = *head_ptr;
+    if (node < head) {
+        node->next = head;
+        head->prev = node;
 
-		*head_ptr = node;
-		return;
-	}
+        *head_ptr = node;
+        return;
+    }
 
-	// ako node treba da stoji negde u sred liste:
-	MemDescr *prev = head;
-	for (MemDescr *curr = head->next; curr; curr = curr->next) {
-		if (node < curr) {
-			prev->next = node;
-			node->prev = prev;
+    // ako node treba da stoji negde u sred liste:
+    MemDescr *prev = head;
+    for (MemDescr *curr = head->next; curr; curr = curr->next) {
+        if (node < curr) {
+            prev->next = node;
+            node->prev = prev;
 
-			curr->prev = node;
-			node->next = curr;
-			return;
-		}
-		prev = curr;
-	}
+            curr->prev = node;
+            node->next = curr;
+            return;
+        }
+        prev = curr;
+    }
 
-	// ako node treba da stoji na kraju liste:
-	prev->next = node;
-	node->prev = prev;
+    // ako node treba da stoji na kraju liste:
+    prev->next = node;
+    node->prev = prev;
 }
 
 void remove(MemDescr **head_ptr, MemDescr *node)
 {
-	if (!head_ptr || !node) // ako ne postoji lista u pitanju ili ako ne postoji node u pitanju
-		return;
+    if (!head_ptr || !node) // ako ne postoji lista u pitanju ili ako ne postoji node u pitanju
+        return;
 
-	MemDescr *head = *head_ptr;
-	if (head == node) {     // ako je node koji se briše početni u listi, njegov sledbenik postaje početni
-		*head_ptr = head->next;
+    MemDescr *head = *head_ptr;
+    if (head == node) {     // ako je node koji se briše početni u listi, njegov sledbenik postaje početni
+        *head_ptr = head->next;
         if (*head_ptr) {
             (*head_ptr)->prev = nullptr;
         }
-		node->next = node->prev = nullptr;
-		return;
-	}
+        node->next = node->prev = nullptr;
+        return;
+    }
 
-	MemDescr *prev = node->prev,
-	         *next = node->next;
+    MemDescr *prev = node->prev,
+            *next = node->next;
 
-	// ako je na bilo kom drugom mestu u listi, samo preveži članove
-	prev->next = next;
+    // ako je na bilo kom drugom mestu u listi, samo preveži članove
+    prev->next = next;
     if (next) {
         next->prev = prev;
     }
 
-	// restovanje pointera
-	node->next = node->prev = nullptr;
+    // restovanje pointera
+    node->next = node->prev = nullptr;
 }
 
 void squash(MemDescr *free)
 {
-	if (!free || free->status != FREE)
-		return;
+    if (!free || free->status != FREE)
+        return;
 
-	MemDescr *next, *curr;
-	curr = (free->prev) ?
-	       free->prev :
-	       free;
+    MemDescr *next, *curr;
+    curr = (free->prev) ?
+           free->prev :
+           free;
 
-	// ako sledeći slobodni segment postoji, i ako se trenutni segment proteže sve do sledećeg, spoji ih
-	while (curr->next && (((char*)curr + curr->size + MEM_BLOCK_SIZE) >= ((char*)(curr->next)))) {
-		next = curr->next;
+    // ako sledeći slobodni segment postoji, i ako se trenutni segment proteže sve do sledećeg, spoji ih
+    while (curr->next && (((char*)curr + curr->size + MEM_BLOCK_SIZE) >= ((char*)(curr->next)))) {
+        next = curr->next;
 
-		curr->size += MEM_BLOCK_SIZE + next->size;
-		curr->next = curr->next->next;
-		if (curr->next)
-			curr->next->prev = curr;
-	}
+        curr->size += MEM_BLOCK_SIZE + next->size;
+        curr->next = curr->next->next;
+        if (curr->next)
+            curr->next->prev = curr;
+    }
 }
