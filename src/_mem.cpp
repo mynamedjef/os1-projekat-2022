e9c043810fa28aba6f167dcd5e7f34ac250747fa
Fedja Mladenovic <fedjamladenovic@gmail.com>
2022-08-16 21:33:05 +0200

    _new.cpp je bio suvišan

--- src/_mem.cpp
+++ src/_mem.cpp
@@ -4,6 +4,9 @@
 
 #include "../lib/mem.h"
 #include "../h/MemoryAllocator.hpp"
+#include "../h/syscall_c.h"
+
+using size_t = decltype(sizeof(0));
 
 void *__mem_alloc(size_t size)
 {
@@ -13,4 +16,24 @@ void *__mem_alloc(size_t size)
 int __mem_free(void *ptr)
 {
     return MemoryAllocator::mem_free(ptr);
+}
+
+void *operator new(size_t n)
+{
+    return mem_alloc(n);
+}
+
+void *operator new[](size_t n)
+{
+    return mem_alloc(n);
+}
+
+void operator delete(void *p) noexcept
+{
+    mem_free(p);
+}
+
+void operator delete[](void *p) noexcept
+{
+    mem_free(p);
 }
